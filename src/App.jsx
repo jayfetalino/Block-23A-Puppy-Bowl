5e4f091aec24e4a719234b9b54c451ebac3708f6
Juvert Jay Fetalino <jayfetalino@jay-fetalino.myfiosgateway.com>
2023-11-11 15:40:33 -0500

    block23

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React, { useState } from 'react'
+
+import { Players } from './features/players/Players';
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+        <Players />
+    </div>
+    );
+  }
+export default App
---
+++ src/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { puppyBowlApi } from "../api/puppyBowlApi";
+
+
+export const store = configureStore({
+  reducer: { 
+    puppyBowlApi: puppyBowlApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(puppyBowlApi.middleware);
+  },
+});
